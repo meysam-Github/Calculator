07f87e30e5dc242ba56b4cadc8bb488baabd3455
Meysam <rmeysam891@gmail.com>
2024-08-18 09:20:22 +0330

    variabel & function

--- Calculator.py
+++ Calculator.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import tkinter.messagebox
 
 #setting
 
@@ -8,6 +9,13 @@ root.title("Calculator")
 root.resizable(width= False, height= False)
 color = "gray"
 root.configure(bg= color)
+
+
+#variabel
+
+num1 = StringVar()
+num2 = StringVar()
+res_value = StringVar()
 
 
 #frames
@@ -25,39 +33,91 @@ top_forth = Frame(root, width= 400, height= 50, bg= color)
 top_forth.pack(side= TOP)
 
 
+#function
+
+def errorMsg(ms):
+    if ms == "error":
+        tkinter.messagebox.showerror("Error", "somthing went wrong")
+        
+    elif ms == "division zero error":
+        tkinter.messagebox.showerror("division Error", "can not divide by zero")
+    
+
+def plus():
+    try:
+        value = float(num1.get()) + float(num2.get())
+        res_value.set(value)
+        
+    except:
+        errorMsg("error")
+        
+        
+def minus():
+    try:
+        value = float(num1.get()) - float(num2.get())
+        res_value.set(value)
+        
+    except:
+        errorMsg("error")
+        
+
+def mul():
+    try:
+        value = float(num1.get()) * float(num2.get())
+        res_value.set(value)
+        
+    except:
+        errorMsg("error")
+
+def div():
+    if num2.get() == '0':
+        errorMsg("division zero error")
+        
+    try:
+        value = float(num1.get()) / float(num2.get())
+        res_value.set(value)
+        
+    except:
+        errorMsg("error")
+        
+
 # buttons
 
-btn_plus = Button(top_third, text= "+", width= 10,  highlightbackground=color)
+btn_plus = Button(top_third, text= "+", width= 10,  highlightbackground=color,
+                  command= lambda: plus())
 btn_plus.pack(side= LEFT, padx= 10, pady= 10)
 
-btn_minus = Button(top_third, text= "-", width= 10, highlightbackground=color)
+btn_minus = Button(top_third, text= "-", width= 10, highlightbackground=color,
+                   command= lambda: minus())
 btn_minus.pack(side= LEFT, padx= 10, pady= 10)
 
-btn_mul = Button(top_third, text= "x", width= 10,highlightbackground=color)
+btn_mul = Button(top_third, text= "x", width= 10,highlightbackground=color,
+                 command= lambda: mul())
 btn_mul.pack(side= LEFT, padx= 10, pady= 10)
 
-btn_div = Button(top_third, text= "/", width= 10,highlightbackground=color)
+btn_div = Button(top_third, text= "/", width= 10,highlightbackground=color,
+                 command= lambda: div())
 btn_div.pack(side= LEFT, padx= 10, pady= 10)
 
 
 # entries and labels
 
-label_first_num = Label(top_first, text= "input number 1 : ", bg= color)
+label_first_num = Label(top_first, text= "input number 1 : ", bg= color,)
 label_first_num.pack(side= LEFT, padx= 10, pady= 10)
 
-first_num = Entry(top_first, highlightbackground= color)
+first_num = Entry(top_first, highlightbackground= color, textvariable= num1)
 first_num.pack(side= LEFT)
 
 label_second_num = Label(top_second, text= "input number 2 : ", bg= color)
 label_second_num.pack(side= LEFT, padx= 10, pady= 10)
 
-second_num = Entry(top_second, highlightbackground= color)
+second_num = Entry(top_second, highlightbackground= color, textvariable= num2)
 second_num.pack(side= LEFT)
 
 res = Label(top_forth, text= "Result", bg= color)
 res.pack(side= LEFT, padx= 10, pady= 10)
 
-res_num = Entry(top_forth, highlightbackground= color)
+res_num = Entry(top_forth, highlightbackground= color, textvariable= res_value)
 res_num.pack(side= LEFT)
 
 
